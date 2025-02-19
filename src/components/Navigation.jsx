74ff4db2fe52b1ba3d02f0b577106cc10c772b9c
unknown <nqnhu6124@gmail.com>
2025-02-19 21:01:23 +0700

    Fix template, add features

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,16 +7,13 @@ const Navigation = () => {
     <nav className="nav">
       <h1 className="logo">GYM FITNESS</h1>
       <ul className="nav-links">
-        <li><Link to="/features" className="nav-link">FEATURES</Link></li>
-        <li><Link to="/about" className="nav-link">ABOUT</Link></li>
-        <li><Link to="/services" className="nav-link">SERVICES</Link></li>
-        <li><Link to="/gallery" className="nav-link">GALLERY</Link></li>
-        <li><Link to="/testimonials" className="nav-link">TESTIMONIALS</Link></li>
-        <li><Link to="/team" className="nav-link">TEAM</Link></li>
-        <li><Link to="/contact" className="nav-link">CONTACT</Link></li>
+        <li><a href="#feature" className="nav-link">ABOUT US</a></li>
+        <li><a href="#services" className="nav-link">SERVICES</a></li>
+        <li><a href="#packages" className="nav-link">COURSES</a></li>
+        <li><a href="#contact" className="nav-link">CONTACT</a></li>
       </ul>
     </nav>
   );
 };
 
-export default Navigation; 
+export default Navigation;
